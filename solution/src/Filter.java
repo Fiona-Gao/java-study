ce99cc42eb1abb610b2710d321b5766a762a3fd1
Fiona-Gao <xiaojiu19933@163.com>
2022-06-02 15:07:50 +0800

    数组去重移动及合并

--- solution/src/Filter.java
+++ solution/src/Filter.java
@@ -1,4 +1,8 @@
-public class Filter {
+public class ArrayTest {
+    /*
+    删除有序数组中重复数字
+    https://leetcode.cn/problems/remove-duplicates-from-sorted-array/
+     */
     public int removeDumplicates(int[] a){
         int n = 0;
         for (int i = 0; i < a.length; i++){
@@ -8,5 +12,39 @@ public class Filter {
             }
         }
         return n;
+    }
+    /*
+    数组中0数字后移
+    https://leetcode.cn/problems/move-zeroes/
+     */
+    public int[] moveZeroes(int[] nums) {
+        int n = 0;
+        for(int i=0; i < nums.length; i++){
+            if (nums[i] != 0) {
+                nums[n] = nums[i];
+                if (n != i){
+                    nums[i] = 0;
+                }
+                n++;
+            }
+        }
+        return nums;
+    }
+    /*
+    合并两个有序数组
+    https://leetcode.cn/problems/merge-sorted-array/submissions/
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        for (int k = m + n - 1; k >= 0; k--){
+            if (j < 0 || (i >= 0 && nums1[i] >= nums2[j])){
+                nums1[k] = nums1[i];
+                i --;
+            }else {
+                nums1[k] = nums2[j];
+                j --;
+            }
+        }
     }
 }
--- solution/src/Main.java
+++ solution/src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Filter f = new Filter();
-        int[] a = new int[]{1,1,2,3,4,5,5,5};
-        int n =f.removeDumplicates(a);
-        System.out.println(n);
+        int[] a = new int[]{1,1,0,3,4,0,5,5};
+        int[] b =f.moveZeroes(a);
+        for (int i = 0; i < b.length; i++){
+            System.out.println(b[i]);
+        }
+
     }
 }
